3914614e6a59d1a6b9b870b594858c5e53502363
luoluoa <2916703365@pp.com>
2022-04-02 16:18:10 +0800

    初始化了环境文件配置

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    open: false,
+    hot: true,
+    port: 9999,
+    host: '127.0.0.1',
+    proxy: {
+      [process.env.VUE_APP_BASE_URL]: {
+        target: process.env.VUE_APP_SERVE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          [`^${process.env.VUE_APP_BASE_URL}`]: ''
+        }
+      }
+    }
+  }
+}
